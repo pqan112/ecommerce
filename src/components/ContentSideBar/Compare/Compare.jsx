48c8983935a3445e1357b3a92af9a8c8112c1cd8
An Pham <pqan@tma.com.vn>
2024-10-24 17:58:24 +0700

    sidebar-compare-ui

--- src/components/ContentSideBar/Compare/Compare.jsx
+++ src/components/ContentSideBar/Compare/Compare.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { TfiReload } from 'react-icons/tfi'
+
+import HeaderSidebar from '../components/HeaderSidebar/HeaderSidebar'
 import styles from './styles.module.scss'
+import ItemProduct from '../components/ItemProduct/ItemProduct'
+import Button from '@components/Button/Button'
 
 function Compare() {
-    return <div>Compare</div>
+    const { container } = styles
+    return (
+        <div className={container}>
+            <div>
+                <HeaderSidebar
+                    icon={<TfiReload style={{ fontSize: '30px' }} />}
+                    title='COMPARE'
+                />
+                <ItemProduct />
+            </div>
+            <Button content={'VIEW COMPARE'} />
+        </div>
+    )
 }
 
 export default Compare
---
+++ src/components/ContentSideBar/components/HeaderSidebar/HeaderSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+function HeaderSidebar({ icon, title }) {
+    const { container } = styles
+
+    return (
+        <div className={container}>
+            {icon}
+            <div>{title}</div>
+        </div>
+    )
+}
+
+export default HeaderSidebar
